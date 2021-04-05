68bb5b94c7276a7b79e2df24c24bdb57d45bb8ce
JD <jherlehy@uwo.ca>
2021-04-04 20:24:35 -0400

    Update Soul_Crusher_Motor_Test.ino

--- Soul_Crusher_Motor_Test/Soul_Crusher_Motor_Test.ino
+++ Soul_Crusher_Motor_Test/Soul_Crusher_Motor_Test.ino
@@ -1,54 +1,46 @@
 #define servoLeft 25
 #define servoRight 23
 
-#define yellowLeftA 26
-#define yellowLeftB 4
-#define yellowRightA 21
-#define yellowRightB 22
+#define yellowLeft 21 //Sda
+#define yellowRight 22 //Sclk
 
-#define pot1 A4
-#define pot2 A7
+#define pot2 A4
 
 int WheelSpeed;
 int ServoPos;
 
 void setup() {
+  Serial.begin(9600);
+  
   // put your setup code here, to run once:
-  ledcAttachPin(yellowLeftA, 1);
-  ledcAttachPin(yellowLeftB, 2);
-  ledcAttachPin(yellowRightA, 3);
-  ledcAttachPin(yellowRightB, 4);
+  ledcAttachPin(yellowLeft, 1);
+  ledcAttachPin(yellowRight, 2);
 
   ledcAttachPin(servoLeft, 5);
   ledcAttachPin(servoRight, 6);
   
   ledcSetup(1, 20000, 8);
   ledcSetup(2, 20000, 8);
-  ledcSetup(3, 20000, 8);
-  ledcSetup(4, 20000, 8);
   ledcSetup(5, 50, 16);
   ledcSetup(6, 50, 16);
 }
 
 void loop() {
-  WheelSpeed = map(analogRead(pot1), 0, 4096, 130, 255);
-  ServoPos = map(analogRead(pot2), 0, 4096, 0, 60);
+  ServoPos = map(analogRead(pot2), 0, 4096, 11, 88);
 
+  Serial.print(ServoPos);
+  Serial.print("\n");
   ledcWrite(5, DDP(ServoPos));
-  ledcWrite(6, DDP(ServoPos));
+  ledcWrite(6, DDP(90 - ServoPos));
   
-  if(WheelSpeed >= 135){ //make a zone of full stop
-    ledcWrite(1, WheelSpeed);
-    ledcWrite(2, 0);
-    ledcWrite(3, WheelSpeed);
-    ledcWrite(4, 0);
+  //if(WheelSpeed >= 135){ //make a zone of full stop
+    ledcWrite(1, 255);
+    ledcWrite(2, 255); //opposiate +/- to force oppisate rotation hardwire
     
-  } else {
+  /*} else {
     ledcWrite(1, 0);
-    ledcWrite(2, 0);
-    ledcWrite(3, 0);
-    ledcWrite(4, 0);    
-  }
+    ledcWrite(2, 0);   
+  }*/
 }
 
 long DDP(int deg) { //degrees to PWM
